3eaaedda6b14942c2345c5c60085121978820211
MasonEmura <memura@csumb.edu>
2020-10-11 21:17:43 -0700

    Initial commit

---
+++ ShootEmUp3/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+   public GameObject enemy;
+   
+    void Start()
+    {
+        for(float i = 0; i < 10; i++)
+        {
+            for(float j = 0; j < 10; j++)
+            {
+                Instantiate(enemy);
+                enemy.transform.position = new Vector2(i, j);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      Debug.Log("Ouch!");
+    }
+}
